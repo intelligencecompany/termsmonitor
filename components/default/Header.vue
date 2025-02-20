9384b29ea980c7975650ce4eb9ac4f4a6dd4c1c7
Gijs Segerink <gijs@trust-ai.nl>
2025-02-20 19:59:40 +0100

    header

--- components/default/Header.vue
+++ components/default/Header.vue
@@ -1,9 +1,13 @@
 <template>
     <v-app-bar app class="app-bar" elevate-on-scroll>
+        
       <v-container class="max-width">
-        <v-row align="center" justify="space-between">
-            <v-toolbar-title>Terms Monitor</v-toolbar-title>
-            <v-spacer></v-spacer>
+        <v-row align="center">
+            <router-link to="/" class="d-flex align-center no-underline">
+                <v-img src="/assets/speedometer.svg" max-height="40" max-width="40" contain></v-img>
+                <v-toolbar-title class="ml-2">Terms Monitor</v-toolbar-title>
+            </router-link>
+            <v-spacer />
             <v-btn text class="d-none d-md-flex" to="/product">Product</v-btn>
             <v-btn text class="d-none d-md-flex" to="/pricing">Pricing</v-btn>
             <v-btn text class="d-none d-md-flex" to="/contact">Contact</v-btn>
@@ -31,3 +35,18 @@
       </v-container>
     </v-app-bar>
 </template>
+
+<style scoped>
+.ml-2 {
+  margin-left: 8px;
+}
+
+.no-underline {
+  text-decoration: none;
+  color: inherit;
+}
+
+.v-app-bar {
+  overflow: visible;
+}
+</style>
